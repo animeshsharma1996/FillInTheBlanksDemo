4bb4ffaa0c353649432d7d2f92c7f5019dfe6ab0
Animesh Sharma <animeshsharma1996@gmail.com>
2022-04-02 19:50:06 +0100

    Correct pick up and throw of blank blocks on the Blank Actor

---
+++ FillTheBlanks/Source/FillTheBlanks/BlankActor.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlankActor.h"
+
+// Called when the game starts or when spawned
+void ABlankActor::Initialise(int32 blankIndex, FString blankString)
+{
+	blankWordIndex = blankIndex;
+	blankWordString = blankString;
+}
+
+
---
+++ FillTheBlanks/Source/FillTheBlanks/BlankActor.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BlankActor.generated.h"
+
+UCLASS()
+class FILLTHEBLANKS_API ABlankActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	UFUNCTION()
+		void Initialise(int32 blankIndex, FString blankString);
+	UFUNCTION()
+		FString GetBlankWordString() { return blankWordString; }
+
+protected:
+	UPROPERTY()
+		int32 blankWordIndex;
+	UPROPERTY()
+		FString blankWordString;
+};
